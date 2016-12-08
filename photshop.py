9784ca33b4282ff961704f7f370d541f67d906b9
minigonche <minigonche@minigonche-macbook.local>
2016-12-08 14:46:30 -0500

    Some final changes

--- photshop.py
+++ photshop.py
@@ -58,11 +58,54 @@ def run_photoshop(input_folder, output_folder, dimensions, merge_option):
     ii.matrix_image_to_frames(np.absolute(L), dimensions, l_out , 'low_rank_')
     ii.matrix_image_to_frames(np.absolute(S), dimensions, s_out , 'sparse_')	
     print('ok')
+
+#Method that runs mean or median after the images have been processed by robust PCA
+def run_photoshop_after(input_folder_pca, input_folder_original, output_folder, dimensions, merge_option):
+
+	M_1 = ii.folder_to_matrix(input_folder_pca)
+
+	if(merge_option.upper() == 'MEAN'):
+		print('Mean')
+		mode = np.matrix(np.apply_along_axis(lambda x: np.mean(x.T), 1, M_1)).T
+	else:    
+		print('Mode')
+		mode = np.matrix(np.apply_along_axis(lambda x: stats.mode(x.T)[0], 1, M_1))
+
+	L = mode
+	for i in range(M_1.shape[1]-1):
+		L = np.hstack((L,mode))
+
+	M = ii.folder_to_matrix(input_folder_original) 
+	S = np.absolute(M - L)
+
+	#Creates the output folder
+	if not os.path.exists(output_folder):
+		os.makedirs(output_folder)
+
+	m_out = output_folder + '/' + 'M'
+	l_out = output_folder + '/' + 'L'
+	s_out = output_folder + '/' + 'S'
+	#Creates the output subfolderers
+	if not os.path.exists(m_out):
+		os.makedirs(m_out)
+
+	if not os.path.exists(l_out):
+		os.makedirs(l_out)
+
+	if not os.path.exists(s_out):
+		os.makedirs(s_out)
+
+	print('saving images...')
+	ii.matrix_image_to_frames(np.absolute(M), dimensions, m_out , 'matrix_')
+	ii.matrix_image_to_frames(np.absolute(L), dimensions, l_out , 'low_rank_')
+	ii.matrix_image_to_frames(np.absolute(S), dimensions, s_out , 'sparse_')	
+	print('ok')    
+
     
     
 if __name__ == "__main__":
 	
-	run_photoshop('frames_all', 'frames__ps_out', (90,160), 'MODE')    
+	run_photoshop_after('background','frames_all', 'frames_final', (90,160), 'Mean')    
         
     
     
